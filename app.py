4c5de72831ffa48e6587b5fcfcfa120a6b0cf007
NuevoNombre <nuevo@email.com>
2025-10-23 12:24:19 -0500

    Solucion final: Correccion CI (PYTHONPATH) y Dockerfile subido

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
+# Inicializa la aplicación Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # El contenido que espera el test_app.py
     return "Hola Mundo"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000)
+    # Importante: Docker requiere escuchar en 0.0.0.0
+    # y el puerto 8080 (o el que se configure en el Dockerfile)
+    app.run(host='0.0.0.0', port=8080)
