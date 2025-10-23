9ea253ec7608784c405ea892c5a3474d42c99b55
NuevoNombre <nuevo@email.com>
2025-10-23 12:05:10 -0500

    Tercer Commit: Test corregido. CI en verde

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,14 +6,13 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     
-    # ✅ CORRECCIÓN: Se espera correctamente "Hola Mundo"
+    # ✅ CORRECCIÓN: Ahora espera correctamente "Hola Mundo"
     assert response.data == b"Hola Mundo"
     
     assert response.status_code == 200
 
-# Añade un segundo test simple (requisito del examen)
+# Segundo test
 def test_status_code():
     client = app.test_client()
     response = client.get('/')
-    # Comprueba si el código de estado es 200, un tipo de prueba diferente
     assert response.status_code == 200
